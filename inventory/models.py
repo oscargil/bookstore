7047189fdadc98dce5c21dec8034aa39d090edf0
Oscar Gil <yosoyeloscar@gmail.com>
2024-01-12 09:52:23 +0100

    More attributes for Book

--- inventory/models.py
+++ inventory/models.py
@@ -3,6 +3,8 @@ from django.db import models
 class Book(models.Model):
     title = models.CharField(max_length=255)
     author = models.CharField(max_length=255)
+    isbn = models.CharField(max_length=13, unique=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
     quantity = models.IntegerField(default=0)
 
     def __str__(self):
